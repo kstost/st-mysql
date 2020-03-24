33bebe0a8368e41cb2cc38ad0d0d8a03a366f367
kstost <monogatree@gmail.com>
2020-03-25 03:02:35 +0900

    first commit

---
+++ st-mysql.js
@@ -0,0 +1,50 @@
+function query(task) {
+    if (arguments.length > 1) {
+        task = Array.from(arguments);
+    }
+    let pool = this;
+    let STR = 'string';
+    return new Promise(r => {
+        (async () => {
+            let result;
+            try {
+                let con = await pool.getConnection(async conn => conn);
+                try {
+                    await con.beginTransaction();
+                    if (typeof task === STR) { task = [task, []]; }
+                    let ntask = [];
+                    for (let i = 0; i < task.length; i++) {
+                        if (typeof task[i] === STR) {
+                            let query = task[i];
+                            let value;
+                            if (task[i + 1] && (typeof task[i + 1] !== STR)) {
+                                value = task[i + 1];
+                            } else {
+                                value = [];
+                            }
+                            ntask.push([query, value]);
+                        }
+                    }
+                    task = ntask;
+                    for (let i = 0; i < task.length; i++) {
+                        let tak = task[i];
+                        let resq = await con.query(tak[0], tak[1]);
+                        if (!result) { result = []; }
+                        result[result.length] = resq[0];
+                    }
+                    await con.commit();
+                } catch (err) {
+                    await con.rollback();
+                    result = err;
+                }
+                con.release();
+            } catch (err) {
+                result = err;
+            }
+            r(result);
+        })();
+    });
+}
+module.exports = function (pool) {
+    return query.bind(pool);
+};
