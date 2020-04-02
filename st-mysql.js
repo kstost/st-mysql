d8c7fbdb9b41f89596b0f89f0d8811723887ea23
kst <kstost@gmail.com>
2020-04-03 01:09:26 +0900

    Update st-mysql.js

--- st-mysql.js
+++ st-mysql.js
@@ -1,3 +1,24 @@
+var Iconv = require('iconv').Iconv;
+
+let charset;
+let encode;
+
+function l2u(content) {
+   if (encode && (typeof content === 'string')) {
+      return new Iconv(encode, charset).convert(content).toString(encode);
+   } else {
+      return content;
+   }
+}
+
+function u2l(content) {
+   if (encode && (typeof content === 'string')) {
+      return new Iconv(charset, encode).convert(content).toString(encode);
+   } else {
+      return content;
+   }
+}
+
 function query(task) {
    if (arguments.length > 1) {
       task = Array.from(arguments);
@@ -28,8 +49,20 @@ function query(task) {
                task = ntask;
                for (let i = 0; i < task.length; i++) {
                   let tak = task[i];
+                  if (encode) {
+                     tak[0] = u2l(tak[0]);
+                     tak[1] = tak[1].map(a => u2l(a));
+                  }
                   let resq = await con.query(tak[0], tak[1]);
                   if (!result) { result = []; }
+                  if (encode) {
+                     let keys;
+                     for (let j = 0; j < resq[0].length; j++) {
+                        let il = resq[0][j];
+                        if (!keys) { keys = Object.keys(il); }
+                        keys.forEach(key => { il[key] = l2u(il[key]); })
+                     }
+                  }
                   result[result.length] = resq[0];
                }
                await con.commit();
@@ -49,7 +82,17 @@ module.exports = function (pool) {
    let promise = pool && pool.constructor.name === 'PromisePool';
    let object = pool && pool.constructor.name === 'Object';
    if (!promise && object && pool.host) {
+      if (pool && pool.encode) {
+         charset = pool.charset;
+         encode = pool.encode;
+         delete pool.encode;
+      }
       pool = require('mysql2/promise').createPool(pool);
    }
-   return query.bind(pool);
+   let newf = query.bind(pool);
+   if (encode) {
+      newf.l2u = l2u;
+      newf.u2l = u2l;
+   }
+   return newf;
 };
